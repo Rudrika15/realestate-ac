2e4d62d719ca5707b8fa05c24281c6999ba7772a
ravirajsinh <ravirajsinh.m.gohil@gmail.com>
2025-03-19 19:39:06 +0530

    multi db

--- models/User.js
+++ models/User.js
@@ -1,63 +1,66 @@
-const { DataTypes } = require('sequelize')
-const { db1 } = require('../config/database')
+const { DataTypes } = require("sequelize");
+const { db1 } = require("../config/database");
 
 const User = db1.define(
-  'User',
+  "User",
   {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
-      primaryKey: true
+      primaryKey: true,
     },
     userName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     status: {
-      type: DataTypes.ENUM('Active', 'Inactive'),
-      defaultValue: 'Active'
+      type: DataTypes.ENUM("Active", "Inactive"),
+      defaultValue: "Active",
     },
     passcode: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     authPasscode: {
       type: DataTypes.STRING,
-      allowNull: true
+      allowNull: true,
     },
     isDeleted: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
     },
     isLocked: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
     },
     createdBy: {
       type: DataTypes.INTEGER,
       allowNull: true,
       references: {
-        model: 'User', // Ensure it matches the model name OR use 'users' if explicitly defined
-        key: 'id'
+        model: "User", // Ensure it matches the model name OR use 'users' if explicitly defined
+        key: "id",
       },
-      onDelete: 'SET NULL',
-      onUpdate: 'CASCADE'
+      onDelete: "SET NULL",
+      onUpdate: "CASCADE",
     },
     updatedBy: {
       type: DataTypes.INTEGER,
-      allowNull: true
-    }
+      allowNull: true,
+    },
   },
   {
-    tableName: 'users', // Explicitly setting the table name
-    timestamps: true
+    tableName: "Users", // Explicitly setting the table name
+    timestamps: true,
   }
-)
+);
 
 // Define associations in an `associate` function to prevent circular dependencies
-User.associate = models => {
-  User.belongsTo(models.User, { foreignKey: 'createdBy', as: 'creator' })
-  User.belongsToMany(models.Role, { through: 'UserRole', foreignKey: 'userId' })
-}
+User.associate = (models) => {
+  User.belongsTo(models.User, { foreignKey: "createdBy", as: "creator" });
+  User.belongsToMany(models.Role, {
+    through: "UserRole",
+    foreignKey: "userId",
+  });
+};
 
-module.exports = { User }
+module.exports = { User };
